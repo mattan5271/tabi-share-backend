4cb74c7b2f1490c7e42c7061378222ecc092de64
mattan5271 <mattan.system.engineering@gmail.com>
2022-03-24 14:20:18 +0900

    🔧 デプロイエラー対策

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,8 @@ lock "~> 3.17.0"
 
 set :application, "tabi-share-backend"
 set :repo_url, "git@github.com:mattan5271/tabi-share-backend.git"
-set :puma_service_unit_name, "puma.service"
+set :branch, "main"
+# set :puma_service_unit_name, "puma.service"
 
 set :deploy_to, "/home/ec2-user/tabi-share-backend"
 set :rbenv_ruby, "3.1.0"
