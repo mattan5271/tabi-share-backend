e96520ea8a07e0eb0c25b77515ebbc78764c3021
mattan5271 <mattan.system.engineering@gmail.com>
2022-03-25 22:45:39 +0900

    🔧 本番環境でのcorsの設定見直し

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://tabi-share-frontend.vercel.app/"
+    if Rails.env.production?
+      origins "https://tabi-share-frontend.vercel.app"
+    else
+      origins "http://localhost:3000"
+    end
 
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
