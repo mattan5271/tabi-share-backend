751f588182c689e8d68cd150cda05464a1c25dbd
mattan5271 <mattan.system.engineering@gmail.com>
2022-04-07 09:59:09 +0900

    🚀 Capistrano設定変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,4 +7,9 @@ set :deploy_to, "/home/ec2-user/tabi-share-v2-backend"
 set :rbenv_ruby, "3.1.0"
 set :linked_files, %w[config/master.key .env]
 set :puma_service_unit_name, "puma.service"
+set :ssh_options, {
+  keys: %w[~/.ssh/id_rsa ~/.ssh/tabi-share-v2.pem],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
 append :linked_dirs, "log", "public", "tmp"
